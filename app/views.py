71e3457958364380a970c652b9ef460551b0cd47
Amp <amp@amperture.com>
2016-10-01 08:40:59 +0000

    We can now get access tokens from StreamLabs

--- app/views.py
+++ app/views.py
@@ -1,11 +1,16 @@
 from flask import render_template, flash, redirect, session, url_for, \
         request, g
 
+import requests as req
+
 from app import app
 from datetime import datetime
+from config import STREAMLABS_CLIENT_ID, STREAMLABS_CLIENT_SECRET
 
 from .forms import RegisterForm
 from .models import User
+
+streamlabs_api_url = 'https://www.twitchalerts.com/api/v1.0/'
 
 @app.route('/')
 @app.route('/index')
@@ -29,3 +34,29 @@ def login():
             title = 'Login',
             form=form,
             )
+
+@app.route('/sl_authorize')
+def sl_authorize():
+    token = streamlabs_api_url + 'token'
+
+    sl_authorize_code = request.args.get('code')
+
+
+    tipcall = {
+                'grant_type'    : 'authorization_code',
+                'client_id'     : STREAMLABS_CLIENT_ID,
+                'client_secret' : STREAMLABS_CLIENT_SECRET,
+                'code'          : sl_authorize_code,
+                'redirect_uri'  : 'http://cs.amperture.com:5000/sl_authorize'
+    }
+    headers = []
+    print token
+
+    check = req.post(token, data=tipcall, headers=headers).json()
+    print "Hello!"
+    print check
+    print "World!"
+
+    return "Hello World!"
+
+
--- config.py
+++ config.py
@@ -6,6 +6,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 CSRF_ENABLED = True
 SECRET_KEY = 'try-and-guess-you-chode'
 
+# Streamlabs Keys, These are only for the testapp, replace when in production
+STREAMLABS_CLIENT_ID = 'JD8Mwed8eJ1S1LgwqfCSCccbSKGtsr0FuVfk35Gw'
+STREAMLABS_CLIENT_SECRET = 'IrLNl9zYTu5MQwUUQqsC9KGfyC31RtzdPThQ0gyU'
+
 SQLALCHEMY_DATABASE_URI = ('sqlite:///' + os.path.join(basedir, 'app.db') +
                                        '?check_same_thread=False')
 
