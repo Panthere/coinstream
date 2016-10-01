151753754fac1a88815e29d28a8eb499df91c678
Amp <amp@amperture.com>
2016-10-01 06:13:48 +0000

    Database up and running

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from hashlib import md5
 
-from app import ap, db
+from app import app, db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -28,6 +28,9 @@ class User(db.Model):
             backref='user',
             lazy='dynamic')
 
+    def __repr__(self):
+        return'<User %r>' %(self.nickname)
+
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
@@ -43,3 +46,5 @@ class Transaction(db.Model):
     twi_user = db.Column(db.String(25))
     twi_message = db.Column(db.String(255))
 
+    def __repr__(self):
+        return '<Transaction %r>' %(self.tx_id)
--- config.py
+++ config.py
@@ -3,7 +3,11 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-CSRF_ENABLED = TRUE
+CSRF_ENABLED = True
 SECRET_KEY = 'try-and-guess-you-chode'
 
-SQLALCHEMY_DATABASE_URI = 
+SQLALCHEMY_DATABASE_URI = ('sqlite:///' + os.path.join(basedir, 'app.db') +
+                                       '?check_same_thread=False')
+
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
