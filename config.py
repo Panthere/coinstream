b1587569620ce4abb7ce993d6c83f699ef2d692b
Amp <amp@amperture.com>
2016-10-01 05:37:19 +0000

    Working server beginnings

---
+++ config.py
@@ -0,0 +1,9 @@
+# -*- coding: utf8 -*-
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+CSRF_ENABLED = TRUE
+SECRET_KEY = 'try-and-guess-you-chode'
+
+SQLALCHEMY_DATABASE_URI = 
