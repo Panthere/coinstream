f364f1577541656a79ddfddd6dcd7de5e54d6bca
Amp <amp@amperture.com>
2016-10-01 05:36:52 +0000

    database management

---
+++ db_create.py
@@ -0,0 +1,17 @@
+#!flask/bin/python
+
+# db_create script taken from Github user Miguel Grinberg
+# github.com/miguelgrinberg
+
+from migrate.versioning import api
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+from app import db
+import os.path
+db.create_all()
+if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
+    api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+else:
+    api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO,
+        api.version(SQLALCHEMY_MIGRATE_REPO))
