534c9a7cc539ca21a2f3c65e10db8e28b61ad692
Amp <amp@amperture.com>
2016-10-21 04:02:15 +0000

    Starting up with Bootstrap templating

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_qrcode import QRcode
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 QRcode(app)
+Bootstrap(app)
 
 from app import views, models
